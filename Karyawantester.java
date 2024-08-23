68b9180ee6b628a5fc60a0ad8559bd65485af19f
nurazlinamemey <nur_sari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 13:44:16 +0700

    Add files via upload

--- Karyawantester.java
+++ Karyawantester.java
@@ -1,6 +1,21 @@
 //Driver Class
+
+import java.util.Scanner;
+
 public class Karyawantester {
     public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("input nama karyawan");
+      String nama = input.nextLine();
+      System.out.println("Inputan unit karyawan");
+      String unit = input.nextLine();
+      System.out.println("Input alamt karyawan");
+      String alamat = input.nextLine();
+      System.out.println("Inputan ID karyawan");
+      int id = input.nextInt();
+      
+      Karyawann keyza = new Karyawann(234,"keyza","BK","tulungagung");
+
         //object
         //Class object = new Constructor
         Karyawann a = new Karyawann(1, "pandu", "TU", "BANYUWANGI");
@@ -9,6 +24,7 @@ public class Karyawantester {
         Karyawann d = new Karyawann(4, "donita", "KUIKULUM", "JOGJA");
         Karyawann e = new Karyawann(5, "april", "KESISWAAN", "PALU");
 
+        
         a.print();
         b.print();
         c.print();
